560fc09ca2f37c251585274def5b21a42f6a008d
Armin Behnamnia <arminbehnamnia@gmail.com>
2017-10-18 00:15:23 +0330

    Database Connection Addded

---
+++ SAN Test Project/src/com/sanproject/model/exceptions/DatabaseConnectionException.java
@@ -0,0 +1,24 @@
+package com.sanproject.model.exceptions;
+
+import java.sql.SQLException;
+
+public class DatabaseConnectionException extends SQLException {
+    public final static int ERR_NO_DRIVER = 0;
+    public final static int ERR_CONNECTION_NOT_ESTABLISHED = 1;
+    private int type = -1;
+    public DatabaseConnectionException(int type){
+        super();
+        this.type = type;
+    }
+    @Override
+    public String getMessage(){
+        switch (type){
+            case ERR_NO_DRIVER:
+                return "No driver detected.";
+            case ERR_CONNECTION_NOT_ESTABLISHED:
+                return "Trying to do query before establishing connection.";
+            default:
+                return "Unknown error.";
+        }
+    }
+}
